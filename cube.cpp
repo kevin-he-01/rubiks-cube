637ad6adeafb5abeedc129955abe968c5ef1fd96
Kevin He <32375681+kevin-he-01@users.noreply.github.com>
2023-08-08 08:29:03 -0700

    CubeDB

--- cube.cpp
+++ cube.cpp
@@ -1,7 +1,7 @@
 // Pocket cube enumeration
 // Inspired by https://medium.com/@bradenripple/enumerating-all-possible-combinations-of-a-pocket-cube-using-golang-ad80d7af23b
 #include <iostream>
-// #include <unordered_map>
+#include <unordered_map>
 #include <unordered_set>
 #include <stdint.h>
 #include <string>
@@ -9,6 +9,7 @@
 #include <vector>
 #include <queue>
 #include <utility>
+#include <algorithm>
 
 // Orientation indicator place
 #define ORIENT 8
@@ -26,7 +27,8 @@
 struct move_t {
     uint8_t shuffle[NCORNERS]; // position shuffle
     uint8_t orientation[NCORNERS]; // post-orientation modification (x ORIENT)
-    std::string name;
+    char face;
+    bool inverted;
 };
 
 typedef uint64_t cube_t;
@@ -39,19 +41,22 @@ move_t moves[2*NMOVES] = {
         //              0, 1, 2, 3, 4, 5, 6, 7
         .shuffle =     {2, 0, 3, 1, 4, 5, 6, 7}, // Permutation: (0 1 3 2)
         .orientation = {0, 0, 0, 0, 0, 0, 0, 0},
-        .name = "U",
+        .face = 'U',
+        .inverted = false,
     },
     move_t {
         //               0, 1, 2, 3, 4, 5, 6, 7
         .shuffle =     { 1, 5, 2, 3, 0, 4, 6, 7}, // Permutation: (0 4 5 1)
         .orientation = {O2,O1, 0, 0,O1,O2, 0, 0},
-        .name = "F",
+        .face = 'F',
+        .inverted = false,
     },
     move_t {
         //               0, 1, 2, 3, 4, 5, 6, 7
         .shuffle =     { 4, 1, 0, 3, 6, 5, 2, 7}, // Permutation: (0 2 6 4)
         .orientation = {O1, 0,O2, 0,O2, 0,O1, 0},
-        .name = "R",
+        .face = 'R',
+        .inverted = false,
     },
     // The rest (inverse operations) will be filled in
 };
@@ -93,7 +98,8 @@ void invert_move(move_t &dst, const move_t &src) {
             dst.orientation[i] = 0;
         }
     }
-    dst.name = src.name + "'";
+    dst.face = src.face;
+    dst.inverted = !src.inverted;
 }
 
 void init_inverses() {
@@ -105,7 +111,7 @@ void init_inverses() {
 std::vector<int> stat_cube() {
     // Compute stats on the cube
     std::vector<int> distance_counts; // count of combinations of each distance from origin
-    std::unordered_set<uint64_t> visited;
+    std::unordered_set<cube_t> visited;
     visited.insert(INITIAL_STATE);
     std::queue<std::pair<cube_t, size_t>> bfs_queue;
     bfs_queue.push(std::make_pair(INITIAL_STATE, 0));
@@ -129,6 +135,49 @@ std::vector<int> stat_cube() {
     return distance_counts;
 }
 
+typedef const move_t *movespec;
+typedef std::unordered_map<cube_t, std::pair<movespec, cube_t>> cubedb;
+
+void init_db(cubedb &db) {
+    db.clear();
+    db[INITIAL_STATE] = std::pair<movespec, cube_t>(nullptr, INITIAL_STATE); // Dummy
+    std::queue<cube_t> bfs_queue;
+    bfs_queue.push(INITIAL_STATE);
+    while (!bfs_queue.empty()) {
+        cube_t v = bfs_queue.front();
+        bfs_queue.pop();
+        for (const move_t &move : moves) {
+            auto w = move_cube(v, move);
+            if (db.count(w))
+                continue;
+            db[w] = std::make_pair(&move, v);
+            bfs_queue.push(w);
+        }
+    }
+}
+
+std::vector<movespec> query_db(cubedb &db, cube_t cube) {
+    std::vector<movespec> route;
+    cube_t current = cube;
+    while (db[current].first) {
+        route.push_back(db[current].first);
+        current = db[current].second;
+    }
+    std::reverse(route.begin(), route.end());
+    return route;
+}
+
+void print_route(const std::vector<movespec> &route) {
+    for (movespec spec : route) {
+        // Assume non-null
+        std::cout << spec->face;
+        if (spec->inverted) {
+            std::cout << '\'';
+        }
+        std::cout << ' ';
+    }
+}
+
 void stats_demo() {
     std::vector<int> stats = stat_cube();
     int total = 0;
@@ -140,6 +189,24 @@ void stats_demo() {
     std::cout << "Total combos: " << total << "\n";
 }
 
+void cubedb_demo() {
+    cubedb db;
+    std::cout << "Initializing DB...\n";
+    init_db(db);
+    std::cout << "DB initialized!\n";
+
+    cube_t cube = INITIAL_STATE;
+    std::cout << "Route: "; print_route(query_db(db, cube)); std::cout << "\n";
+    cube = move_cube(cube, moves[0]);
+    std::cout << "Route: "; print_route(query_db(db, cube)); std::cout << "\n";
+    cube = move_cube(cube, moves[1]);
+    std::cout << "Route: "; print_route(query_db(db, cube)); std::cout << "\n";
+    cube = move_cube(cube, moves[2]);
+    std::cout << "Route: "; print_route(query_db(db, cube)); std::cout << "\n";
+    cube = move_cube(cube, moves[2]);
+    std::cout << "Route: "; print_route(query_db(db, cube)); std::cout << "\n";
+}
+
 int main(int argc, char **argv) {
     init_inverses();
     if (argc != 2) {
@@ -149,6 +216,8 @@ int main(int argc, char **argv) {
     std::string command = argv[1];
     if (command == "stats") {
         stats_demo();
+    } else if (command == "cubedb") {
+        cubedb_demo();
     } else {
         std::cerr << "unknown command: " << command << "\n";
         return 2;
